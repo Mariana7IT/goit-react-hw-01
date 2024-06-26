35bfd0468e2b9f15a572711df30357668567633f
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-06-26 15:04:04 +0300

    base layot component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './components/App';
+import Profile from './components/Profile';
+import TransactionHistory from './components/TransactionHistory';
+import FriendList from './components/FriendList';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
-)
+    <Profile />
+    <FriendList /> 
+    <TransactionHistory /> 
+  </>,);
